7b0ae998d2c5a24c33c560e6b5a5380b893743cd
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2023-07-09 10:43:53 +0100

    Fix errors

--- src/prefixCommands/Other/test.js
+++ src/prefixCommands/Other/test.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'test',
     async execute(message) {
-        await message.reply('To remove this command, delete the file under chatCommands/Other/test.js\nOtherwise, use it as an example of how to make your own commands.')
+        await message.reply('To remove this command, delete the file under prefixCommands/Other/test.js\nOtherwise, use it as an example of how to make your own commands.')
     }
 }
