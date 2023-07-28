9ba249895f72a07c8c1d1c8bc422c9354505579f
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2023-07-28 20:18:15 +0100

    Update

--- src/prefixCommands/Other/say.js
+++ src/prefixCommands/Other/say.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'say',
     async execute(message, options = {}) {
         if (!options.args || !typeof options.args === String || options.args.length < 1) return await message.reply({ content: 'Please give me something to say!' });
-        await message.reply({content: `You said ${options.args}\nMessage Array: [${options.argsArray}]` });
+        await message.channel.send({content: `You said ${options.args}\nMessage Array: [${options.argsArray}]\nOutput requested by ${message.author}` });
     }
 }
